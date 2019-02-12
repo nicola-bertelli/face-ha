9913f8a88d34a2e541b4d74ba06a42786547da11
nicola-bertelli <45416528+nicola-bertelli@users.noreply.github.com>
2019-02-12 21:30:16 +0100

    Create fotografia.py

---
+++ riconoscimento/fotografia.py
@@ -0,0 +1,22 @@
+import time
+import picamera
+import yaml
+
+file_configurazione = "/home/pi/riconoscimento/config.yaml"
+with open(file_configurazione, 'r') as ymlfile:
+    configurazione = yaml.load(ymlfile)
+
+risoluzione_x = configurazione['stato_camera']['risoluzione']['x']
+risoluzione_y = configurazione['stato_camera']['risoluzione']['y']
+rotazione = configurazione['stato_camera']['rotazione']['gradi']
+
+camera = picamera.PiCamera()
+camera.resolution = (risoluzione_x, risoluzione_y)
+camera.rotation = (rotazione)
+try:
+    camera.start_preview()
+    time.sleep(10)
+    camera.capture('immagine.jpg')
+    camera.stop_preview()
+finally:
+    camera.close()
